7e1bbaae52e5a5c6416b85b8a7eabd3c8edf6d95
ClearSale <support@clear.sale>
2016-10-12 19:19:05 -0400

    Upload PHP Sample V1.0.0

---
+++ PHP/Order/Entity/Payment.php
@@ -0,0 +1,18 @@
+<?php
+
+class Clearsale_Total_Model_Order_Entity_Payment
+{
+	public $Date;     
+	public $Type;
+	public $Gateway;
+	public $CardNumber;
+	public $CardHolderName;
+	public $CardEndNumber;
+	public $Amount;
+	public $PaymentTypeID;
+	public $CardBin;
+}
+
+?>
+
+
---
+++ PHP/Utils/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+class Config   
+{
+const CONFIG_APIKEY =  '08EC93CE-7700-4D62-A1B0-9061F0730556';
+const CONFIG_CLIENTID ='worldjewels';
+const CONFIG_CLIENTSECRET = 'SBNoy04T49'; 
+const CONFIG_ENVIRONMENT_SANDBOX ='https://sandbox.clearsale.com.br/'; 
+const CONFIG_ENVIRONMENT_PRODUCTION =  'https://integration.clearsale.com.br/'; 
+const CONFIG_ANALYSIS_LOCATION_USA = 'USA';
+const CONFIG_ANALYSIS_LOCATION_BRA = 'BRA';
+const CONFIG_CURRENCY_BRA = 'BRL';
+const CONFIG_CURRENCY_USA = 'USD';
+}
+
+?>
