1e47bb0a8c8ac3c25871aa8812e5b1d2694e1556
ClearSale - Fraud Prevention Solution <support@clear.sale>
2017-10-13 17:13:03 -0400

    Address Pattern

--- PHP/Order/Entity/Address.php
+++ PHP/Order/Entity/Address.php
@@ -2,14 +2,10 @@
 
 class Clearsale_Total_Model_Order_Entity_Address
 {
-	public $Street;
+	public $AddressLine1;
+	public $AddressLine2;
 	public $City;
 	public $State;
 	public $ZipCode;
-	public $County;
 	public $Country;
-	public $Comp;
-	public $Number;
-}
-
 ?>
